7570283aca144ca6e88ac66ab227217ba2db66ce
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-08-02 03:53:31 +0530

    frontend initial setup & shadcn/ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './components/shared/Navbar'
+
+function App() {
+ 
+
+  return (
+    <div className='bg-red-700'>
+     <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,41 @@
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#f83002]">Portal</span>
+          </h1>
+        </div>
+
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+
+          {/* avatar with popover */}
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent>
+              <h1>AVATAR</h1>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
