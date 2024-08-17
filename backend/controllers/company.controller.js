a3516d899c76956ce35367ab48a4cb0bf9c92c9d
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-08-17 20:32:38 +0530

    add : show all company + filter /Updation

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
   try {
@@ -70,28 +72,32 @@ export const getCompanyById = async (req, res) => {
   }
 };
 
-export const updateCompany = async(req,res)=>{
-    try {
-        const {name, description,website,location} = req.body;
-        const file =  req.file;
+export const updateCompany = async (req, res) => {
+  try {
+      const { name, description, website, location } = req.body;
 
-        const updateData = {name, description,website,location};
+      const file = req.file;
+     
+      const fileUri = getDataUri(file);
+      const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+      const logo = cloudResponse.secure_url;
+  
+      const updateData = { name, description, website, location, logo };
 
-        const company = await Company.findByIdAndUpdate(req.params.id,updateData,{new:true});
-        
-        if(!company){
-            return res.status(404).json({
-                message:"Company not found.",
-                success:false
-            })
-        }
+      const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
 
-        return res.status(200).json({
-            message:"Company information updated.",
-            success:true
-        })
+      if (!company) {
+          return res.status(404).json({
+              message: "Company not found.",
+              success: false
+          })
+      }
+      return res.status(200).json({
+          message:"Company information updated.",
+          success:true
+      })
 
-    } catch (error) {
-        console.log(error)
-    }
+  } catch (error) {
+      console.log(error);
+  }
 }
--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,23 +1,39 @@
-import { useNavigate } from "react-router-dom";
-import Navbar from "../shared/Navbar";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
-import CompaniesTable from "./CompaniesTable";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import CompaniesTable from './CompaniesTable'
+import { useNavigate } from 'react-router-dom'
+import useGetAllCompanies from '@/hooks/useGetAllCompanies'
+import { useDispatch } from 'react-redux'
+import { setSearchCompanyByText } from '@/redux/companySlice'
 
 const Companies = () => {
+    useGetAllCompanies();
+    const [input, setInput] = useState("");
     const navigate = useNavigate();
-  return (
-    <div>
-      <Navbar />
-      <div className="max-w-6xl mx-auto my-10">
-        <div className="flex items-center justify-between my-5">
-        <Input className="w-fit" placeholder="Filter by name" />
-        <Button onClick={() => navigate("/admin/companies/create")}>New Company</Button>
-        </div>
-        <CompaniesTable/>
-      </div>
-    </div>
-  );
-};
+    const dispatch = useDispatch();
 
-export default Companies;
+    useEffect(()=>{
+        dispatch(setSearchCompanyByText(input));
+    },[input]);
+    return (
+        <div>
+            <Navbar />
+            <div className='max-w-6xl mx-auto my-10'>
+                <div className='flex items-center justify-between my-5'>
+                    <Input
+                        className="w-fit"
+                        placeholder="Filter by name"
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Button onClick={() => navigate("/admin/companies/create")}>New Company</Button>
+                </div>
+                <CompaniesTable/>
+            </div>
+        </div>
+    )
+}
+
+export default Companies
